9c96436c4bcc46545fd07c03ff4bfd8be0bfb4e0
AnKiT_07 <asfrrll2@gmail.com>
2026-06-28 22:26:43 +0530

    Fixed issue with the error notification auto dismissal and added success notification. Fixed bugs.

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace IDA_VendorFlow
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
